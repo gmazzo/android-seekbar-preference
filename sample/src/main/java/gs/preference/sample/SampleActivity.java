1bf6d0d5174adef4fbabc7ebd69b3d9dea894630
guillermo.mazzola <guillermo.mazzola@globant.com>
2017-02-13 00:59:09 -0300

    Added sample app

---
+++ sample/src/main/java/gs/preference/sample/SampleActivity.java
@@ -0,0 +1,16 @@
+package gs.preference.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+public class SampleActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.sample);
+    }
+
+}
